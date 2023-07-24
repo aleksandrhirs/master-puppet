e1f99427add233cfa8c2ed3f35ed8b44b5d41d61
aleksandr.hirs <aleksandr.hirs@gmail.com>
2023-07-24 18:35:55 +0300

    06-puppet

--- manifests/site.pp
+++ manifests/site.pp
@@ -83,27 +83,9 @@ node master.puppet {
 
 node mineserver.puppet {
 
-  file { '/opt/minecraft':
-    ensure => 'directory',
-  }
-
   package { 'java-17-openjdk-devel':
-    ensure => installed,
+    ensure     => installed,
   }
 
-  exec { 'minecraft_server':
-    command => '/usr/bin/curl -o /opt/minecraft/server.jar https://piston-data.mojang.com/v1/objects/84194a2f286ef7c14ed7ce0090dba59902951553/server.jar',
-    creates => '/opt/minecraft/server.jar',
-  }
-
-  file { 'minecraft_service':
-    path   => '/etc/systemd/system/minecraft.service',
-    source => '/vagrant/minecraft.service.erb',
-    notify => Service['minecraft'],
-  }
-
-  service { 'minecraft':
-    ensure  => running,
-    require => File['minecraft_service'],
-  }
+  include minecraft
 }
